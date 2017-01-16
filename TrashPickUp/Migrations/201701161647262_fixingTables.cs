722410f6f5be18520ea1702cebe4e6cbec938d55
Nik Dries <ninja1704@hotmail.com>
2017-01-16 11:02:40 -0600

    added migrations

---
+++ TrashPickUp/Migrations/201701161647262_fixingTables.cs
@@ -0,0 +1,200 @@
+namespace TrashPickUp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixingTables : DbMigration
+    {
+        public override void Up()
+        {
+
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Street = c.String(),
+                    ApartmentNumber = c.String(),
+                    City = c.String(),
+                    State = c.String(maxLength: 2),
+                    ZipCode = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.ID);
+
+            CreateTable(
+                "dbo.Calendars",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    StartDate = c.DateTime(),
+                    EndDate = c.DateTime(),
+                })
+                .PrimaryKey(t => t.ID);
+
+            //CreateTable(
+            //    "dbo.Employees",
+            //    c => new
+            //    {
+            //        ID = c.Int(nullable: false, identity: true),
+            //        UserID = c.String(maxLength: 128),
+            //        RouteNumber = c.Int(nullable: false),
+            //    })
+            //    .PrimaryKey(t => t.ID)
+            //    .ForeignKey("dbo.AspNetUsers", t => t.UserID)
+            //    .Index(t => t.UserID);
+
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    FirstName = c.String(),
+                    LastName = c.String(),
+                    Address_id = c.Int(),
+                    PickupDay = c.String(),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Addresses", t => t.Address_id)
+                .Index(t => t.Address_id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                {
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+
+            CreateTable(
+                "dbo.PickUpDays",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Day = c.String(),
+                    Status = c.Boolean(nullable: false),
+                    EmployeeID = c.Int(nullable: false),
+                    AddressID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Addresses", t => t.AddressID, cascadeDelete: true)
+                .ForeignKey("dbo.Employees", t => t.EmployeeID, cascadeDelete: true)
+                .Index(t => t.EmployeeID)
+                .Index(t => t.AddressID);
+
+            CreateTable(
+                "dbo.RegisterdUserInfoes",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    PickupDay = c.String(),
+                    MonthlyBill = c.Double(nullable: false),
+                    currentUser_Id = c.String(maxLength: 128),
+                })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.AspNetUsers", t => t.currentUser_Id)
+                .Index(t => t.currentUser_Id);
+
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+
+            CreateTable(
+                "dbo.Routes",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    RouteZipCode = c.Int(nullable: false),
+                    AddressID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Addresses", t => t.AddressID, cascadeDelete: true)
+                .Index(t => t.AddressID);
+
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.Routes", "AddressID", "dbo.Addresses");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.RegisterdUserInfoes", "currentUser_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.PickUpDays", "EmployeeID", "dbo.Employees");
+            DropForeignKey("dbo.PickUpDays", "AddressID", "dbo.Addresses");
+            DropForeignKey("dbo.Employees", "UserID", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUsers", "Address_id", "dbo.Addresses");
+            DropIndex("dbo.Routes", new[] { "AddressID" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.RegisterdUserInfoes", new[] { "currentUser_Id" });
+            DropIndex("dbo.PickUpDays", new[] { "AddressID" });
+            DropIndex("dbo.PickUpDays", new[] { "EmployeeID" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUsers", new[] { "Address_id" });
+            //DropIndex("dbo.Employees", new[] { "UserID" });
+            DropTable("dbo.Routes");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.RegisterdUserInfoes");
+            DropTable("dbo.PickUpDays");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            //DropTable("dbo.Employees");
+            DropTable("dbo.Calendars");
+            DropTable("dbo.Addresses");
+        }
+    }
+}
